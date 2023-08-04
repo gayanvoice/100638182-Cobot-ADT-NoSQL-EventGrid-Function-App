177ca0e7cdcd64bc20978ecfd968401374910c58
gayanvoice <gayan.kuruppu@live.com>
2023-08-04 11:29:12 +0100

    fix: error

--- CobotADTNoSQLEventGridFunctionApp/EventGridFunction.cs
+++ CobotADTNoSQLEventGridFunctionApp/EventGridFunction.cs
@@ -20,30 +20,31 @@ namespace CobotADTNoSQLEventGridFunctionApp
         private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
         private static readonly string cosmosUri = Environment.GetEnvironmentVariable("COSMOS_URI");
         [FunctionName("ProcessADTRoutedDataToNoSql")]
-        public static async Task ProcessADTRoutedDataToNoSqlAsync([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public static async Task ProcessADTRoutedDataToNoSqlAsync([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
         {
-            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential();
-            DigitalTwinsClient digitalTwinsClient = new DigitalTwinsClient(endpoint: new Uri(adtInstanceUrl), credential: defaultAzureCredential);
-            log.LogInformation(eventGridEvent.Data.ToString());
-            log.LogInformation($"ADT service client connection created.");
-            CosmosClient cosmosClient = new CosmosClient(accountEndpoint: cosmosUri, tokenCredential: defaultAzureCredential);
-            log.LogInformation($"Cosmos service client connection created.");
-            log.LogInformation(eventGridEvent.Data.ToString());
-            Database cosmosDatabase = await cosmosClient.CreateDatabaseIfNotExistsAsync(id: "cobotDb");
-            Container cobotContainer = cosmosDatabase.GetContainer(id: "cobotContainer");
-            if (eventGridEvent != null && eventGridEvent.Data != null)
+            try
             {
-                RootModel rootModel = JsonConvert.DeserializeObject<RootModel>(eventGridEvent.Data.ToString());
-                log.LogInformation(JsonConvert.SerializeObject(rootModel, Formatting.Indented));
-                Azure.JsonPatchDocument jsonPatchDocument = new Azure.JsonPatchDocument();
-                string timestamp = DateTime.Now.ToString("yyyyMMddHHmmssffff");
-                log.LogInformation(timestamp);
-                switch (rootModel.Data.ModelId)
+                DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential();
+                DigitalTwinsClient digitalTwinsClient = new DigitalTwinsClient(endpoint: new Uri(adtInstanceUrl), credential: defaultAzureCredential);
+                log.LogInformation(eventGridEvent.Data.ToString());
+                log.LogInformation($"ADT service client connection created.");
+                CosmosClient cosmosClient = new CosmosClient(accountEndpoint: cosmosUri, tokenCredential: defaultAzureCredential);
+                log.LogInformation($"Cosmos service client connection created.");
+                log.LogInformation(eventGridEvent.Data.ToString());
+                Database cosmosDatabase = await cosmosClient.CreateDatabaseIfNotExistsAsync(id: "cobotDb");
+                Container cobotContainer = cosmosDatabase.GetContainer(id: "cobotContainer");
+                if (eventGridEvent != null && eventGridEvent.Data != null)
                 {
-                    case "dtmi:com:Cobot:Cobot;1":
-                        log.LogInformation(rootModel.Data.ModelId);
-                        try
-                        {
+                    RootModel rootModel = JsonConvert.DeserializeObject<RootModel>(eventGridEvent.Data.ToString());
+                    log.LogInformation(JsonConvert.SerializeObject(rootModel, Formatting.Indented));
+                    Azure.JsonPatchDocument jsonPatchDocument = new Azure.JsonPatchDocument();
+                    string timestamp = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+                    log.LogInformation(timestamp);
+                    switch (rootModel.Data.ModelId)
+                    {
+                        case "dtmi:com:Cobot:Cobot;1":
+                            log.LogInformation(rootModel.Data.ModelId);
+
                             Cobot cobot = new(
                                 id: $"cobot-{timestamp}",
                                 timestamp: timestamp,
@@ -54,73 +55,76 @@ namespace CobotADTNoSQLEventGridFunctionApp
                                 partitionKey: new PartitionKey(timestamp)
                             );
                             Console.WriteLine($"Created item:\t{cobotCreatedItem.id}\t{cobotCreatedItem.elapsedTime}\t[{cobotCreatedItem.timestamp}]");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                        break;
-                    case "dtmi:com:Cobot:Payload;1":
-                        //jsonPatchDocument.AppendReplace("/Mass", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Mass")).Value);
-                        //jsonPatchDocument.AppendReplace("/CogX", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogX")).Value);
-                        //jsonPatchDocument.AppendReplace("/CogY", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogY")).Value);
-                        //jsonPatchDocument.AppendReplace("/CogZ", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogZ")).Value);
-                        break;
-                    case "dtmi:com:Cobot:ControlBox;1":
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        //await client.UpdateDigitalTwinAsync("TControlBox", jsonPatchDocument);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Elbow;1":
-                        //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        //jsonPatchDocument.AppendReplace("/X", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/X")).Value);
-                        //jsonPatchDocument.AppendReplace("/Y", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Y")).Value);
-                        //jsonPatchDocument.AppendReplace("/Z", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Z")).Value);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Base;1":
-                        //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Shoulder;1":
-                        //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Wrist1;1":
-                        //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        //await client.UpdateDigitalTwinAsync("TWrist1", jsonPatchDocument);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Wrist2;1":
-                        //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        //await client.UpdateDigitalTwinAsync("TWrist2", jsonPatchDocument);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Wrist3;1":
-                        //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        //await client.UpdateDigitalTwinAsync("TWrist3", jsonPatchDocument);
-                        break;
-                    case "dtmi:com:Cobot:JointLoad:Tool;1":
-                        //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
-                        //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        //jsonPatchDocument.AppendReplace("/X", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/X")).Value);
-                        //jsonPatchDocument.AppendReplace("/Y", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Y")).Value);
-                        //jsonPatchDocument.AppendReplace("/Z", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Z")).Value);
-                        //jsonPatchDocument.AppendReplace("/Rx", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Rx")).Value);
-                        //jsonPatchDocument.AppendReplace("/Ry", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Ry")).Value);
-                        //jsonPatchDocument.AppendReplace("/Rz", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Rz")).Value);
-                        //await client.UpdateDigitalTwinAsync("TTool", jsonPatchDocument);
-                        break;
-                    default:
-                        break;
+
+                            break;
+                        case "dtmi:com:Cobot:Payload;1":
+                            //jsonPatchDocument.AppendReplace("/Mass", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Mass")).Value);
+                            //jsonPatchDocument.AppendReplace("/CogX", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogX")).Value);
+                            //jsonPatchDocument.AppendReplace("/CogY", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogY")).Value);
+                            //jsonPatchDocument.AppendReplace("/CogZ", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogZ")).Value);
+                            break;
+                        case "dtmi:com:Cobot:ControlBox;1":
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            //await client.UpdateDigitalTwinAsync("TControlBox", jsonPatchDocument);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Elbow;1":
+                            //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            //jsonPatchDocument.AppendReplace("/X", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/X")).Value);
+                            //jsonPatchDocument.AppendReplace("/Y", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Y")).Value);
+                            //jsonPatchDocument.AppendReplace("/Z", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Z")).Value);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Base;1":
+                            //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Shoulder;1":
+                            //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Wrist1;1":
+                            //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            //await client.UpdateDigitalTwinAsync("TWrist1", jsonPatchDocument);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Wrist2;1":
+                            //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            //await client.UpdateDigitalTwinAsync("TWrist2", jsonPatchDocument);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Wrist3;1":
+                            //jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            //await client.UpdateDigitalTwinAsync("TWrist3", jsonPatchDocument);
+                            break;
+                        case "dtmi:com:Cobot:JointLoad:Tool;1":
+                            //jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
+                            //jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
+                            //jsonPatchDocument.AppendReplace("/X", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/X")).Value);
+                            //jsonPatchDocument.AppendReplace("/Y", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Y")).Value);
+                            //jsonPatchDocument.AppendReplace("/Z", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Z")).Value);
+                            //jsonPatchDocument.AppendReplace("/Rx", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Rx")).Value);
+                            //jsonPatchDocument.AppendReplace("/Ry", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Ry")).Value);
+                            //jsonPatchDocument.AppendReplace("/Rz", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Rz")).Value);
+                            //await client.UpdateDigitalTwinAsync("TTool", jsonPatchDocument);
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
         }
     }
 }
